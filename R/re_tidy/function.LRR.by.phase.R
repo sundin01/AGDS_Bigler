c48172623c6b2d3c0936e27685efa3eb58ac8724
Patrick Bigler <patrick@macbook-pro.home>
2023-05-14 19:58:23 +0200

    changed the file name

---
+++ R/re_tidy/function.LRR.by.phase.R
@@ -0,0 +1,11 @@
+LRR.by.phase <- function(database){
+  out.print <-knitr::kable(database |>
+    group_by(Phase) |>
+    # Apply both methods and round the result
+    summarise(LRR_Method_1 =
+               round(log(mean(`mean ambient CO2`) /  mean(`mean increased CO2`)), digits = 3),
+              LRR_Method_2 =
+               round(mean(log(`mean ambient CO2`/ `mean increased CO2`)), digits = 3)))
+  # Return the data frame
+  return(out.print)
+}
