5347bed9d76740028100dd9428315522f108e411
Patrick Bigler <patrick@macbook-pro.home>
2023-05-17 13:09:15 +0200

    resolved merge conflict

--- R/airquality/function.regression.calculater.R
+++
@@ -1,6 +0,0 @@
-regression.calculater <- function(dataframe, target, predictor){
-  target.column.nr <- target
-  predictor.column.nr <- predictor
-  lm.model <- lm(unlist(dataframe[target.column.nr]) ~ unlist(dataframe[ predictor.column.nr]))
-  return(summary(lm.model))
-}
