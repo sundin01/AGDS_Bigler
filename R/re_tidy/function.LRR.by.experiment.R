b142c437067cf9c64f0c5805033b240e5875cef9
Patrick Bigler <patrick@stud-86-54.eduroam.unibe.ch>
2023-06-02 12:34:50 +0200

    changed functions
    - table with capture
    - table with h lines

--- R/re_tidy/function.LRR.by.experiment.R
+++ R/re_tidy/function.LRR.by.experiment.R
@@ -1,12 +1,14 @@
-LRR.by.experiment <- function(database){
+LRR.by.experiment <- function(database, tablenumber){
   # Use kable()
   out.print <- knitr::kable(database |>
     group_by(Experiment) |>
     # Apply both methods and round the result
-    summarise(LRR_Method_1 =
-               round(log(mean(`mean ambient CO2`) /  mean(`mean increased CO2`)), digits = 3),
-              LRR_Method_2 =
-                round(mean(log(`mean ambient CO2`/ `mean increased CO2`)), digits = 3)))
+    summarise(LRR_Method_1 = round(log(mean(`mean ambient CO2`) /  mean(`mean increased CO2`)), digits = 3),
+              LRR_Method_2 = round(mean(log(`mean ambient CO2`/ `mean increased CO2`)), digits = 3)),
+              caption = paste("Table ", tablenumber, ": LRR for each Experiment"),
+              align = c("l", "c", "c"), format = "html")|>
+    kable_classic()
   # Return the data frame
   return(out.print)
 }
+
--- R/re_tidy/function.LRR.by.phase.R
+++ R/re_tidy/function.LRR.by.phase.R
@@ -1,11 +1,12 @@
-LRR.by.phase <- function(database){
+LRR.by.phase <- function(database, tablenumber){
   out.print <-knitr::kable(database |>
     group_by(Phase) |>
     # Apply both methods and round the result
-    summarise(LRR_Method_1 =
-               round(log(mean(`mean ambient CO2`) /  mean(`mean increased CO2`)), digits = 3),
-              LRR_Method_2 =
-               round(mean(log(`mean ambient CO2`/ `mean increased CO2`)), digits = 3)))
+    summarise(LRR_Method_1 = round(log(mean(`mean ambient CO2`) /  mean(`mean increased CO2`)), digits = 3),
+              LRR_Method_2 = round(mean(log(`mean ambient CO2`/ `mean increased CO2`)), digits = 3)),
+    caption = paste("Table ", tablenumber, ": LRR for each Phase"),
+    align = c("l", "c", "c"), format = "html")|>
+    kable_classic()
   # Return the data frame
   return(out.print)
 }
