ab6ff642928419c6368e1687c09da618bcea2e88
Patrick Bigler <patrick@macbook-pro.home>
2023-06-04 13:22:16 +0200

    final version

--- R/general/function.visualize.na.values.R
+++ R/general/function.visualize.na.values.R
@@ -1,4 +1,9 @@
-visualize.na.values <- function(database){
+visualize.na.values <- function(database, label = NULL){
+
+  title <- ggdraw() +
+    draw_label(paste("Quality Control:", label),fontface = 'bold',x = 0, hjust = 0) +
+    theme(plot.margin = margin(0, 0, 0, 7))
+
   # Plot the NA with percentage
   plot_1 <- vis_miss(database)
 
@@ -7,14 +12,16 @@ visualize.na.values <- function(database){
 
   # Create a cowplot
   out.plot <- cowplot::plot_grid(plot_1, plot_2)
-  return(out.plot)
+
+  final_plot <- cowplot::plot_grid(title, out.plot, ncol = 1, rel_heights = c(0.1,1))
+
+  return(final_plot)
 }
 
 visualize.na.values.without.groups <- function(database, label = NULL){
   title <- ggdraw() +
     draw_label(paste("Quality Control:", label),fontface = 'bold',x = 0, hjust = 0) +
     theme(plot.margin = margin(0, 0, 0, 7))
-
 
   plot_1 <- vis_miss(database)
 
@@ -23,6 +30,7 @@ visualize.na.values.without.groups <- function(database, label = NULL){
   out.plot <- cowplot::plot_grid(plot_1, plot_2)
 
   final_plot <- cowplot::plot_grid(title, out.plot, ncol = 1, rel_heights = c(0.1,1))
+
   return(final_plot)
 }
 
