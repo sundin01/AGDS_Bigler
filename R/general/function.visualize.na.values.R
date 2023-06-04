a5436d772d161f24d7dab4fc4bce189ef39f6809
Patrick Bigler <patrick@macbook-pro.home>
2023-06-04 13:09:10 +0200

    final version

--- R/general/function.visualize.na.values.R
+++ R/general/function.visualize.na.values.R
@@ -10,11 +10,19 @@ visualize.na.values <- function(database){
   return(out.plot)
 }
 
-visualize.na.values.without.groups <- function(database){
+visualize.na.values.without.groups <- function(database, label = NULL){
+  title <- ggdraw() +
+    draw_label(paste("Quality Control:", label),fontface = 'bold',x = 0, hjust = 0) +
+    theme(plot.margin = margin(0, 0, 0, 7))
+
+
   plot_1 <- vis_miss(database)
 
   plot_2 <- gg_miss_var(database)
 
   out.plot <- cowplot::plot_grid(plot_1, plot_2)
-  return(out.plot)
+
+  final_plot <- cowplot::plot_grid(title, out.plot, ncol = 1, rel_heights = c(0.1,1))
+  return(final_plot)
 }
+
